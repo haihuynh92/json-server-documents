15ef9343c6dd5efac3569a4bb05cb091763bf238
hhuynh30 <hhuynh30@dxc.com>
2021-07-04 21:37:10 +0700

    update

--- main.js
+++ main.js
@@ -47,6 +47,6 @@ router.render = (req, res) => {
  
 // Use default router
 server.use(router)
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running');
 })
